05e1e823e63703d1447d995c1fd1d10b1487dba1
Kinght <1301680879@qq.com>
2024-04-04 15:34:43 +0800

    🐞 fix: 修改深色/浅色主题图标

--- src/layouts/components/Header/RightContent/index.tsx
+++ src/layouts/components/Header/RightContent/index.tsx
@@ -1,5 +1,9 @@
-import { BulbOutlined } from "@ant-design/icons";
-import { SelectLang as UmiSelectLang } from "@umijs/max";
+import {
+  WeatherMoon16Regular as DarkIcon,
+  WeatherSunny16Regular as LightIcon,
+} from "@ricons/fluent";
+import { Icon } from "@ricons/utils";
+import { SelectLang as UmiSelectLang, useModel } from "@umijs/max";
 import React from "react";
 
 import { InitialState } from "@/types";
@@ -19,6 +23,7 @@ export const SelectLang = () => {
 export const Theme = (props: {
   changeState: (exec: (oldData: InitialState) => InitialState) => void;
 }) => {
+  const init = useModel("@@initialState", (v) => v.initialState);
   const handleClick = () => {
     props.changeState((old) => {
       return {
@@ -30,5 +35,21 @@ export const Theme = (props: {
       };
     });
   };
-  return <BulbOutlined onClick={handleClick}></BulbOutlined>;
+  return (
+    <div
+      onClick={handleClick}
+      style={{
+        height: "42px",
+        width: "42px",
+      }}
+    >
+      <Icon>
+        {init.config.isDark ? (
+          <LightIcon style={{ position: "relative", top: "-12px", left: "7px" }} />
+        ) : (
+          <DarkIcon style={{ position: "relative", top: "-12px", left: "7px" }} />
+        )}
+      </Icon>
+    </div>
+  );
 };
